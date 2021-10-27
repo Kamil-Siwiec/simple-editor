3dd4d5402dd878679ce4af9dc9bf50803fd4a810
Kamil Siwiec <kamsiwcode@gmail.com>
2021-10-27 23:18:00 +0100

    add variables

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,9 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO, take a look how it works, and have fun 🚀')
+
+const textarea = document.querySelector('.textarea--js');
+const save = document.querySelector('.load--js');
+const load = document.querySelector('.save--js');
+const clear = document.querySelector('.clear--js');
